a247ee808aef09a7efd815d084a9173aeced2a71
Andrew Halberstadt <ahal@pm.me>
2021-03-27 22:54:28 -0400

    Add ability to autojoin on invite

--- src/bot.rs
+++ src/bot.rs
@@ -1,29 +1,69 @@
 use std::{env, process::exit};
+use tokio::time::{delay_for, Duration};
 
 use matrix_sdk::{
     self, async_trait,
     events::{
-        room::message::{MessageEventContent, TextMessageEventContent},
-        AnyMessageEventContent, SyncMessageEvent,
+        room::{
+            member::{MemberEventContent},
+            message::{MessageEventContent, TextMessageEventContent},
+        },
+        AnyMessageEventContent, StrippedStateEvent, SyncMessageEvent,
     },
     Client, ClientConfig, EventEmitter, JsonStore, SyncRoom, SyncSettings,
 };
 use url::Url;
 
-struct CommandBot {
+struct BotZilla {
     /// This clone of the `Client` will send requests to the server,
     /// while the other keeps us in sync with the server using `sync`.
     client: Client,
 }
 
-impl CommandBot {
+impl BotZilla {
     pub fn new(client: Client) -> Self {
         Self { client }
     }
 }
 
 #[async_trait]
-impl EventEmitter for CommandBot {
+impl EventEmitter for BotZilla {
+    async fn on_stripped_state_member(
+        &self,
+        room: SyncRoom,
+        room_member: &StrippedStateEvent<MemberEventContent>,
+        _: Option<MemberEventContent>,
+    ) {
+        if room_member.state_key != self.client.user_id().await.unwrap() {
+            return;
+        }
+
+        if let SyncRoom::Invited(room) = room {
+            let room = room.read().await;
+            println!("Autojoining room {}", room.room_id);
+            let mut delay = 2;
+
+            while let Err(err) = self.client.join_room_by_id(&room.room_id).await {
+                // retry autojoin due to synapse sending invites, before the
+                // invited user can join for more information see
+                // https://github.com/matrix-org/synapse/issues/4345
+                eprintln!(
+                    "Failed to join room {} ({:?}), retrying in {}s",
+                    room.room_id, err, delay
+                );
+
+                delay_for(Duration::from_secs(delay)).await;
+                delay *= 2;
+
+                if delay > 3600 {
+                    eprintln!("Can't join room {} ({:?})", room.room_id, err);
+                    break;
+                }
+            }
+            println!("Successfully joined room {}", room.room_id);
+        }
+    }
+
     async fn on_room_message(&self, room: SyncRoom, event: &SyncMessageEvent<MessageEventContent>) {
         if let SyncRoom::Joined(room) = room {
             let msg_body = if let SyncMessageEvent {
@@ -84,16 +124,16 @@ async fn login_and_sync(
     // If the `StateStore` finds saved state in the location given the initial sync will
     // be skipped in favor of loading state from the store
     client.sync_once(SyncSettings::default()).await.unwrap();
-    // add our CommandBot to be notified of incoming messages, we do this after the initial
+    // add BotZilla to be notified of incoming messages, we do this after the initial
     // sync to avoid responding to messages before the bot was running.
     client
-        .add_event_emitter(Box::new(CommandBot::new(client.clone())))
+        .add_event_emitter(Box::new(BotZilla::new(client.clone())))
         .await;
 
     // since we called `sync_once` before we entered our sync loop we must pass
     // that sync token to `sync`
     let settings = SyncSettings::default().token(client.sync_token().await.unwrap());
-    // this keeps state from the server streaming in to CommandBot via the EventEmitter trait
+    // this keeps state from the server streaming in to BotZilla via the EventEmitter trait
     client.sync(settings).await;
 
     Ok(())
