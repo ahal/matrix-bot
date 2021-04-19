e7a1faf763e7ec40bb3b542860b54ce353e68c07
Andrew Halberstadt <ahal@pm.me>
2021-04-18 23:50:50 -0400

    Create a test for autojoin functionality

--- src/bot.rs
+++ src/bot.rs
@@ -17,8 +17,7 @@ use serde::Deserialize;
 use tokio::time::{sleep, Duration};
 use url::Url;
 
-#[derive(Deserialize)]
-#[derive(Default)]
+#[derive(Deserialize, Default)]
 struct MatrixBotConfig<'a> {
     homeserver: &'a str,
     username: &'a str,
@@ -28,7 +27,7 @@ struct MatrixBotConfig<'a> {
 
 impl<'a> MatrixBotConfig<'a> {
     fn from_config(content: &'a str) -> MatrixBotConfig<'a> {
-        let config : MatrixBotConfig = toml::from_str(content).unwrap();
+        let config: MatrixBotConfig = toml::from_str(content).unwrap();
         config
     }
 }
@@ -42,12 +41,11 @@ pub struct MatrixBot {
 
 impl MatrixBot {
     pub async fn new(config_path: &str) -> Result<Self, matrix_sdk::Error> {
-        tracing_subscriber::fmt::init();
         let config_contents = fs::read_to_string(config_path).expect("Error reading config file!");
         let config = MatrixBotConfig::from_config(&config_contents);
 
         let statedir = match config.statedir {
-            Some(a)=> PathBuf::from(a),
+            Some(a) => PathBuf::from(a),
             None => {
                 match directories::ProjectDirs::from("ca", "ahal", "matrix-bot") {
                     Some(dirs) => dirs.data_dir().to_path_buf(),
@@ -127,7 +125,9 @@ impl EventHandler for MatrixBot {
                 // https://github.com/matrix-org/synapse/issues/4345
                 eprintln!(
                     "Failed to join room {} ({:?}), retrying in {}s",
-                    room.room_id(), err, delay
+                    room.room_id(),
+                    err,
+                    delay
                 );
 
                 sleep(Duration::from_secs(delay)).await;
@@ -157,35 +157,92 @@ impl EventHandler for MatrixBot {
 mod tests {
     use super::*;
 
+    use matrix_sdk::{identifiers::room_id, Client};
+    use matrix_sdk_test::{test_json, EventBuilder};
+    use mockito::mock;
+    use serde_json::json;
     use std::fs::File;
     use std::io::Write;
-    use matrix_sdk_test::test_json;
-    use mockito::mock;
     use tempfile::tempdir;
+    use url::form_urlencoded::byte_serialize;
 
-    #[tokio::test]
-    async fn login() {
+    async fn get_client() -> Client {
         let homeserver = mockito::server_url();
         let _m = mock("POST", "/_matrix/client/r0/login")
             .with_status(200)
             .with_body(test_json::LOGIN.to_string())
             .create();
 
-        let config = &format!(r#"
+        let config = &format!(
+            r#"
             homeserver = "{}"
             username = "user"
             password = "password"
-        "#, homeserver);
+            statedir = "{}"
+        "#,
+            homeserver,
+            tempdir().unwrap().path().to_str().unwrap()
+        );
 
         let dir = tempdir().unwrap();
         let path = dir.path().join("config.toml");
         let mut file = File::create(&path).unwrap();
         writeln!(file, "{}", config).unwrap();
 
-        let bot = MatrixBot::new(&path.to_str().unwrap())
-            .await
-            .unwrap();
-        let logged_in = bot.client.logged_in().await;
+        let bot = MatrixBot::new(&path.to_str().unwrap()).await.unwrap();
+        let client = bot.client.clone();
+        client.set_event_handler(Box::new(bot)).await;
+        client
+    }
+
+    #[tokio::test]
+    async fn login() {
+        let client = get_client().await;
+        let logged_in = client.logged_in().await;
         assert!(logged_in, "Bot should be logged in");
+    }
+
+    #[tokio::test]
+    async fn autojoin() {
+        let client = get_client().await;
+        let room_id = room_id!("!SVkFJHzfwvuaIEawgC:localhost");
+        let room = client.get_invited_room(&room_id);
+        assert!(room.is_none());
+
+        let event = json!({
+            "content": {
+                "displayname": "example",
+                "membership": "join"
+            },
+            "event_id": "$151800140517rfvjc:localhost",
+            "membership": "join",
+            "origin_server_ts": 0,
+            "sender": "@example:localhost",
+            "state_key": "@cheeky_monkey:matrix.org",
+            "type": "m.room.member"
+        });
+
+        let sync_response_json = EventBuilder::default()
+            .add_custom_invited_event(&room_id, event)
+            .build_json_sync_response();
+
+        let m1 = mock(
+            "GET",
+            mockito::Matcher::Regex(r"^/_matrix/client/r0/sync.*".to_string()),
+        )
+        .with_status(200)
+        .with_body(sync_response_json.to_string())
+        .create();
+
+        let encoded_room_id: String = byte_serialize(room_id.as_bytes()).collect();
+        let url = format!("/_matrix/client/r0/rooms/{}/join", encoded_room_id);
+        let m2 = mock("POST", url.as_str())
+            .with_status(200)
+            .with_body(json!({ "room_id": &room_id }).to_string())
+            .create();
+
+        client.sync_once(SyncSettings::default()).await.unwrap();
+        m1.assert();
+        m2.assert();
     }
 }
